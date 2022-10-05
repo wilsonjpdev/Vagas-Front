5872c78a958def386159c67abc9e9f4320f104c3
wilsonjp <wilsonjp.dev@outlook.com>
2022-10-05 13:29:41 -0300

    Inicio do projeto

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <topo-pagina />
+    <conteudo-pagina />
+  </div>
+</template>
+
+<script>
+import ConteudoPagina from './components/layouts/ConteudoPagina.vue';
+import TopoPagina from './components/layouts/TopoPagina.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TopoPagina,
+    ConteudoPagina
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
