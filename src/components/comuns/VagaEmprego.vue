d219bfd43c7b03f4980d9abef89e2fbb9637f1a7
wilsonjp <wilsonjp.dev@outlook.com>
2022-10-07 16:59:01 -0300

    Criacao dos cards de vagas dinamicos

---
+++ src/components/comuns/VagaEmprego.vue
@@ -0,0 +1,33 @@
+<template>
+      <div class="row mt-5">
+      <div class="col">
+        <div class="card">
+          <div class="card-header bg-dark text-white">{{ titulo }}</div>
+          <div class="card-body">
+            <p>{{ descricao }}</p>
+          </div>
+          <div class="card footer">
+            <small class="text-muted">Salário: {{ salario }} | Modalidade: {{ modalidade }} | Tipo: {{ tipo }} | Publicação: {{ publicacao }}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: [
+      'titulo',
+      'descricao',
+      'salario',
+      'modalidade',
+      'tipo',
+      'publicacao'
+    ]
+}
+</script>
+
+<style>
+
+</style>
--- src/components/views/HomePage.vue
+++ src/components/views/HomePage.vue
@@ -1,39 +1,120 @@
 <template>
   <div class="container py-4">
-    <pesquisar-vaga />
+    <div class="row">
+      <div class="col">
+        <pesquisar-vaga />
+      </div>
+    </div>
+
+    <div class="row mt-5" v-for="(vaga, index) in vagas" :key="index">
+      <div class="col">
+        <vaga-emprego 
+          :titulo="vaga.titulo" 
+          :descricao="vaga.descricao" 
+          :salario="vaga.salario" 
+          :modalidade="vaga.modalidade" 
+          :tipo="vaga.tipo" 
+          :publicacao="vaga.publicacao" />
+      </div>
+    </div>
+
     <div class="row mt-5">
       <div class="col-4">
-        <indicador-vaga titulo="Vagas abertas" indicador="25" bg="bg-dark" color="text-white" />
+        <indicador-vaga
+          titulo="Vagas abertas"
+          indicador="25"
+          bg="bg-dark"
+          color="text-white"
+        />
       </div>
       <div class="col-4">
-        <indicador-vaga titulo="Profissionais cadastrados" indicador="125" bg="bg-dark" color="text-white" />
+        <indicador-vaga
+          titulo="Profissionais cadastrados"
+          indicador="125"
+          bg="bg-dark"
+          color="text-white"
+        />
       </div>
       <div class="col-4">
-        <indicador-vaga titulo="Visitantes online" :indicador="usuariosOnline" bg="bg-light" color="text-dark" />
+        <indicador-vaga
+          titulo="Visitantes online"
+          :indicador="usuariosOnline"
+          bg="bg-light"
+          color="text-dark"
+        />
       </div>
     </div>
   </div>
 </template>
     
 <script>
-  import PesquisarVaga from '@/components/comuns/PesquisarVaga.vue';
-  import IndicadorVaga from '@/components/comuns/IndicadorVaga.vue';
+import PesquisarVaga from "@/components/comuns/PesquisarVaga.vue";
+import IndicadorVaga from "@/components/comuns/IndicadorVaga.vue";
+import VagaEmprego from "@/components/comuns/VagaEmprego.vue";
 
-    export default {
-      components: {
-        PesquisarVaga,
-        IndicadorVaga
+export default {
+  components: {
+    PesquisarVaga,
+    IndicadorVaga,
+    VagaEmprego,
+  },
+  data: () => ({
+    usuariosOnline: 0,
+    vagas: [
+      {
+        titulo: "Analista Programador PHP Pleno",
+        descricao:
+          "Profissional com conhecimentos em PHP, Laravel e MySQL. Necessário 3 anos de experiências. Atuará na manutenção de sistemas legados da empresa.",
+        salario: 6000,
+        modalidade: "Home Office",
+        tipo: "PJ",
+        publicacao: "2021-10-10",
       },
-      data: () => ({
-        usuariosOnline: 0
-      }),
-      methods: {
-        getUsuariosOnline() {
-          this.usuariosOnline = Math.floor(Math.random() * 101)
-        }
+      {
+        titulo: "Programador JavaScript Angular",
+        descricao:
+          "Profissional com conhecimentos avançados em JavaScript e Angular.",
+        salario: 5000,
+        modalidade: "Presencial",
+        tipo: "CLT",
+        publicacao: "2021-10-07",
       },
-      created() {
-        setInterval(this.getUsuariosOnline, 1000)
-      }
-    }
+      {
+        titulo: "Programador JavaScript Vue",
+        descricao:
+          "Profissional com conhecimentos avançados em JavaScript e Vue.",
+        salario: 5000,
+        modalidade: "Home Office",
+        tipo: "CLT",
+        publicacao: "2021-10-06",
+      },
+      {
+        titulo: "Analista de Banco de Dados Sênior",
+        descricao:
+          "Domí­nio dos bancos de dados SQL Server, Oracle, Postgre e MySQL",
+        salario: 9000,
+        modalidade: "Presencial",
+        tipo: "PJ",
+        publicacao: "2021-10-06",
+      },
+      {
+        titulo: "Programador Web Júnior",
+        descricao:
+          "Conhecimentos básicos em HTML, CSS, JavaScript, Bootstrap, PHP e MySQL",
+        salario: 3000,
+        modalidade: "Presencial",
+        tipo: "CLT",
+        publicacao: "2021-10-05",
+      },
+    ],
+  }),
+  methods: {
+    getUsuariosOnline() {
+      this.usuariosOnline = Math.floor(Math.random() * 101);
+    },
+  },
+  created() {
+    setInterval(this.getUsuariosOnline, 1000);
+  },
+};
 </script>
